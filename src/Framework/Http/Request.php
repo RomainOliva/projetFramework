5bffbcf8477d566d665383f53c15a8bc24918d0f
Romain Oliva <olivar.info@gmail.com>
2016-01-05 15:34:17 +0100

    Ajout des test unitaire pour le path, la method et le scheme

--- src/Framework/Http/Request.php
+++ src/Framework/Http/Request.php
@@ -1,76 +1,112 @@
 <?php
-
 namespace Framework\Http;
-
 class Request
 {
     const GET = 'GET';
     const POST = 'POST';
     const PUT = 'PUT';
     const PATCH = 'PATCH';
-    const OPTION = 'OPTION';
+    const OPTIONS = 'OPTIONS';
+    const CONNECT = 'CONNECT';
     const TRACE = 'TRACE';
     const HEAD = 'HEAD';
     const DELETE = 'DELETE';
-
     const HTTP = 'HTTP';
     const HTTPS = 'HTTPS';
-
-    private $method;            //Méthode - Verbe
-    private $scheme;            //Protocol
-    private $schemeVersion;     //Version - 1.1 2.0 ...
-    private $path;              //Url
-    private $headers;            //Ex: tableau associatif d'entete
-    private $body;              //Body
-
+    private $method;
+    private $scheme;
+    private $schemeVersion;
+    private $path;
+    private $headers;
+    private $body;
     /**
      * Constructor.
      *
-     * @param string $method            The HTTP verb
-     * @param string $path              The resource path on the server
-     * @param string $scheme            The protocol name (HTTP or HTTPS)
-     * @param string $schemeVersion     The scheme version (ie: 1.0, 1.1 or 2.0)
-     * @param array  $headers           An associative array of headers
-     * @param string $body              The request content
+     * @param string $method        The HTTP verb
+     * @param string $path          The resource path on the server
+     * @param string $scheme        The protocole name (HTTP or HTTPS)
+     * @param string $schemeVersion The scheme version (ie: 1.0, 1.1 or 2.0)
+     * @param array  $headers       An associative array of headers
+     * @param string $body          The request content
      */
     public function __construct($method, $path, $scheme, $schemeVersion, array $headers = [], $body = '')
     {
-        $this->method = $method;
+        $this->setMethod($method);
         $this->path = $path;
-        $this->scheme = $scheme;
+        $this->setScheme($scheme);
         $this->schemeVersion = $schemeVersion;
         $this->headers = $headers;
         $this->body = $body;
-
     }
-
+    private function setMethod($method)
+    {
+        $methods = [
+            self::GET,
+            self::POST,
+            self::PUT,
+            self::PATCH,
+            self::OPTIONS,
+            self::CONNECT,
+            self::TRACE,
+            self::HEAD,
+            self::DELETE,
+        ];
+        if (!in_array($method, $methods)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Method %s is not supported and must be one of %s.',
+                $method,
+                implode(', ', $methods)
+            ));
+        }
+        $this->method = $method;
+    }
     public function getMethod()
     {
         return $this->method;
     }
-
     public function getScheme()
     {
         return $this->scheme;
     }
-
     public function getSchemeVersion()
     {
         return $this->schemeVersion;
     }
-
     public function getPath()
     {
         return $this->path;
     }
-
     public function getHeaders()
     {
         return $this->headers;
     }
-
     public function getBody()
     {
         return $this->body;
+    }
+    private function setScheme($scheme)
+    {
+        $schemes = [ self::HTTP, self::HTTPS ];
+        if (!in_array($scheme, $schemes)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Scheme %s is not supported and must be one of %s.',
+                $scheme,
+                implode(', ', $schemes)
+            ));
+        }
+        $this->scheme = $scheme;
+    }
+
+    private function setSchemeVersion($version)
+    {
+        $versions = [ self::Version_1_0, self::Version_1_1 ];
+        if (!in_array($scheme, $schemes)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Scheme %s is not supported and must be one of %s.',
+                $scheme,
+                implode(', ', $schemes)
+            ));
+        }
+        $this->scheme = $scheme;
     }
 }
--- tests/Framework/Http/RequestTest.php
+++ tests/Framework/Http/RequestTest.php
@@ -1,20 +1,81 @@
 <?php
-
 namespace Tests\Framework\Http;
-
 use Framework\Http\Request;
-
 class RequestTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCreateRequestInstance()
+    /**
+     * @dataProvider provideValidHttpScheme
+     */
+    public function testSupportedHttpScheme($scheme)
     {
-        $request = new Request(Request::GET, '/', Request::HTTP, '1.1');
-
-        $this->assertSame(Request::GET, $request->getMethod());
-        $this->assertSame('/', $request->getPath(), 'Pour mettre un commentaire si assert complexe');
+        new Request('GET', '/', $scheme, '1.1');
+    }
+    public function provideValidHttpScheme()
+    {
+        return [
+            [ Request::HTTP ],
+            [ Request::HTTPS ],
+        ];
+    }
+    /**
+     * @expectedException \InvalidArgumentException
+     * @dataProvider provideInvalidHttpScheme
+     */
+    public function testUnsupportedHttpScheme($scheme)
+    {
+        new Request('GET', '/', $scheme, '1.1');
+    }
+    public function provideInvalidHttpScheme()
+    {
+        return [
+            [ 'FTP' ],
+            [ 'SFTP' ],
+            [ 'SSH' ],
+        ];
+    }
+    /**
+     * @expectedException \InvalidArgumentException
+     * @dataProvider provideInvalidHttpMethod
+     */
+    public function testUnsupportedHttpMethod($method)
+    {
+        new Request($method, '/', 'HTTP', '1.1');
+    }
+    public function provideInvalidHttpMethod()
+    {
+        return [
+            [ 'FOO' ],
+            [ 'BAR' ],
+            [ 'BAZ' ],
+            [ 'PURGE' ],
+            [ 'TOTO' ],
+        ];
+    }
+    /**
+     * @dataProvider provideRequestParameters
+     */
+    public function testCreateRequestInstance($method, $path)
+    {
+        $request = new Request($method, $path, Request::HTTP, '1.1');
+        $this->assertSame($method, $request->getMethod());
+        $this->assertSame($path, $request->getPath());
         $this->assertSame(Request::HTTP, $request->getScheme());
         $this->assertSame('1.1', $request->getSchemeVersion());
         $this->assertEmpty($request->getHeaders());
         $this->assertEmpty($request->getBody());
+    }
+    public function provideRequestParameters()
+    {
+        return [
+            [ Request::GET,     '/'              ],
+            [ Request::POST,    '/home'          ],
+            [ Request::PUT,     '/foo'           ],
+            [ Request::PATCH,   '/bar'           ],
+            [ Request::OPTIONS, '/options'       ],
+            [ Request::CONNECT, '/lol'           ],
+            [ Request::TRACE,   '/contact'       ],
+            [ Request::HEAD,    '/fr/article/42' ],
+            [ Request::DELETE,  '/cgv'           ],
+        ];
     }
 }
