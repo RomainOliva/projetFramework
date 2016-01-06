58d1afd31113da5761bbcea14c9712ee669a8c14
Romain Oliva <olivar.info@gmail.com>
2016-01-06 10:56:59 +0100

    [Framework] introduced two new exception types.

---
+++ src/Framework/Http/MalformedHttpHeaderException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RomainMac
+ * Date: 06/01/2016
+ * Time: 10:53
+ */
+
+namespace Framework\Http;
+
+
+class MalformedHttpHeaderException extends \RuntimeException
+{
+
+}
